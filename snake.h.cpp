8db99c7fbe259f0ca402e0ddd519e594c3c7801b
Hua-rookie <messihua@126.com>
2020-04-10 22:49:32 +0800

    初稿

--- snake.h.cpp
+++ snake.h.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-struct Point
+struct Point//每个节点对应每一身段
 {
     int x;
     int y;
@@ -10,7 +10,7 @@ struct Point
     Point(Point *ptr=NULL):link(ptr){};
     Point(int x,int y,Point *ptr=NULL):x(x),y(y),link(ptr){};
 };
-enum
+enum//设定按键与蛇移动方向的对应关系
 {
     Up='w',
     Down='s',
@@ -25,11 +25,11 @@ public:
     Snake(Snake &s);
     ~Snake();
     
-    bool Insert(int i,int &x,int &y);
-    bool Remove(int i,int &x,int &y);
+    bool Insert(int i,int &x,int &y);//插入新身段，用于蛇移动或者蛇吃东西
+    bool Remove(int i,int &x,int &y);//删除身段，用于蛇移动或者其他原因导致蛇身变短
     bool Death();//等有了wall，参数表里加入wall的引用
     bool Eat();
-    bool Move();//现在和eat还有点冲突，丞待解决
+    bool Move();//里面实现蛇头的移动，身子的跟进等等，现在可能和eat还有点冲突，丞待解决
 protected:
     Point *first;
     int length;
