d2846751579b4f97aeeb19ae2015a795bd704df3
Hua-rookie <messihua@126.com>
2020-04-14 12:08:34 +0800

    Update snake.h
    加入最新apple头文件

--- snake.h
+++ snake.h
@@ -1,6 +1,8 @@
+#ifndef snake_h
+#define snake_h
 #include <iostream>
 #include"wall.h"
-#include"food.h"
+#include"apple.h"
 using namespace std;
 const int defaultSize=100;
 struct Point
@@ -18,21 +20,24 @@ enum//设定按键与蛇移动方向的对应关系
 class Snake
 {
 public:
-    Snake(int sz=defaultSize);
+    Snake(int sz=defaultSize) {Head=NULL;maxSize=defaultSize;}//用于调试
+    //Snake(Wall &tempWall,Apple &apple,int sz=defaultSize);//正式版
     ~Snake();
     bool InitSnake();
-    bool Death();
+    bool Death();//等有了wall，参数表里加入wall的引用
     bool Eat();
     bool Move(char pre_direction,char cur_direction);
     bool Crawl(char pre_direction,char cur_direction);//实现不同难度之下，move，death，eat函数的调配
     int getLength() {return length;}
-    wall &m_wall;
-    apple &m_apple;
+    Point *GetHead() {return Head;}
+    //Wall &m_wall;
+    //Apple &m_apple;
 protected:
     Point *Head;//指向蛇头
     int length;//蛇的长度
     int maxSize;//蛇的最大长度
     int tailx;
-	int taily;
+    int taily;
 };
 //用数组实现
+#endif /* snake_h */
